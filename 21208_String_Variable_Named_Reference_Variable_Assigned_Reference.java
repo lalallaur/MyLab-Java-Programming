ebcb88bbef0ef89b327a3d314cd03f30b0d09b7c
laur <112447573+lalallaur@users.noreply.github.com>
2023-09-14 02:14:57 -0700

    Create 21208_String_Variable_Named_Reference_Variable_Assigned_Reference.java

---
+++ 21208_String_Variable_Named_Reference_Variable_Assigned_Reference.java
@@ -0,0 +1,4 @@
+//Given a String variable named line1 and given a Scanner reference variable stdin that has been assigned a reference to a Scanner object, read the next line from stdin and save it in line1. (Do not concern yourself with any possible exceptions here-- assume they are handled elsewhere.)
+Scanner keyboard = new Scanner(System.in);
+System.out.println("line1: ");
+line1 = keyboard.nextLine();
