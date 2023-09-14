6f91ddde86293b7b6c8d9a146fec3ac34e6949dc
laur <112447573+lalallaur@users.noreply.github.com>
2023-09-14 02:03:46 -0700

    Create 20758_Double_Values_Integer_Variables_Assign_Double_Variable.java

---
+++ 20758_Double_Values_Integer_Variables_Assign_Double_Variable.java
@@ -0,0 +1,3 @@
+//Calculate the average (as a double) of the values contained in the integer variables num1, num2, num3 and assign that average to the double variable avg.
+//Assume the variables num1, num2, and num3 have been declared and assigned values, and the variable avg declared.
+avg = (double) (num1 + num2 + num3) / 3;
