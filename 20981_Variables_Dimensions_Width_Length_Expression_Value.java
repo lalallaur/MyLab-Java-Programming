e0261a7e0b9b92f3c488b9349662befd16840e66
laur <112447573+lalallaur@users.noreply.github.com>
2023-09-14 01:57:50 -0700

    Create 20981_Variables_Dimensions_Width_Length_Expression_Value.java

---
+++ 20981_Variables_Dimensions_Width_Length_Expression_Value.java
@@ -0,0 +1,2 @@
+//The dimensions (width and length) of room1 have been read into two variables: width1 and length1. The dimensions of room2 have been read into two other variables: width2 and length2. Write a single expression whose value is the total floor-space area of the two rooms.
+(width1 * length1) + (width2 * length2)
