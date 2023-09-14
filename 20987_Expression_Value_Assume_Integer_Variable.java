efe58a8af2e5cfdec00a738fc1f55d9f0e9307ce
laur <112447573+lalallaur@users.noreply.github.com>
2023-09-14 02:06:07 -0700

    Create 20987_Expression_Value_Assume_Integer_Variable.java

---
+++ 20987_Expression_Value_Assume_Integer_Variable.java
@@ -0,0 +1,2 @@
+//Assume that children is an integer variable containing the number of children in a community and that families is an integer variable containing the number of families in the same community. Write an expression whose value is the average number of children per family.
+(double) children / families
