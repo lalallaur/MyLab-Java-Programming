c40d13accfee07efade74d60e1587c3d97d30072
laur <112447573+lalallaur@users.noreply.github.com>
2023-09-14 01:55:00 -0700

    Create 20546_Double_Variables_Expression_Net_Weight.java

---
+++ 20546_Double_Variables_Expression_Net_Weight.java
@@ -0,0 +1,2 @@
+//You are given two double variables, already declared and assigned values, named totalWeight, containing the weight of a shipment, and weightOfBox, containing the weight of the box in which a product is shipped. Write an expression that calculates the net weight of the product.
+totalWeight - weightOfBox
