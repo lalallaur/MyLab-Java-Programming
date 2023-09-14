5d994cd2b088de17436ab7365d0f238d56f61c77
laur <112447573+lalallaur@users.noreply.github.com>
2023-09-14 02:08:13 -0700

    Create 20523_Declare_Constant_Value_Constant.java

---
+++ 20523_Declare_Constant_Value_Constant.java
@@ -0,0 +1,2 @@
+//Declare a constant MONTHS_IN_DECADE, whose value is the value of the constant MONTHS_IN_YEAR (already declared) multiplied by 10.
+final int MONTHS_IN_DECADE = MONTHS_IN_YEAR * 10;
