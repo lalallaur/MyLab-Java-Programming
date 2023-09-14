d966880507c7af693245bf1fa57230aba7d62dbd
laur <112447573+lalallaur@users.noreply.github.com>
2023-09-14 02:13:02 -0700

    Create 20606_Given_Int_Variable_Statement_Integer_Value.java

---
+++ 20606_Given_Int_Variable_Statement_Integer_Value.java
@@ -0,0 +1,4 @@
+//Given an int variable datum that has already been declared, write a few statements that read an integer value from standard input into this variable.
+Scanner keyboard = new Scanner(System.in);
+System.out.println("Enter a value: ");
+datum = keyboard.nextInt();
