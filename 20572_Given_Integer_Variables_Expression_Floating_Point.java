8088c28016683799f3d9600e91345cb4b2ef2fc4
laur <112447573+lalallaur@users.noreply.github.com>
2023-09-14 02:01:32 -0700

    Create 20572_Given_Integer_Variables_Expression_Floating_Point.java

---
+++ 20572_Given_Integer_Variables_Expression_Floating_Point.java
@@ -0,0 +1,2 @@
+//Given two integer variables distance and speed, write an expression that divides distance by speed using floating point arithmetic, i.e. a fractional result should be produced.
+(double) distance / speed
